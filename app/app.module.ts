2634222878621189c0e4453cd286d4d7e4e1c5cc
onischenkoa <artem.onischenko@ligazakon.ua>
2017-06-16 13:47:16 +0300

    study service

--- app/app.module.ts
+++ app/app.module.ts
@@ -6,10 +6,18 @@ import { EventListComponent } from './events/event-list.componet'
 import {EventThumbnailComponent} from  './events/event-thumbnail.component'
 import {NavBarComponent} from './nav/navbar.component'
 
+import {EventService} from './events/shared/event.service'
+import {ToastrService} from './common/toaster.service'
+
 
 @NgModule({
     imports:[BrowserModule],
-    declarations: [EventsAppComponent, EventListComponent, EventThumbnailComponent, NavBarComponent],
+    declarations: [
+        EventsAppComponent,
+        EventListComponent,
+        EventThumbnailComponent,
+        NavBarComponent],
+    providers: [EventService, ToastrService],
     bootstrap: [EventsAppComponent]
 })
 export class AppModule {}

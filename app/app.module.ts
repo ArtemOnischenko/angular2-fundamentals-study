da0f5afb5442ebad217069c06e06549c214e128b
onischenkoa <artem.onischenko@ligazakon.ua>
2017-06-16 10:37:48 +0300

    initial commit

---
+++ app/app.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core'
+import { BrowserModule } from  '@angular/platform-browser'
+
+import { EventsAppComponent } from './events-app.component'
+import { EventListComponent } from './events/event-list.componet'
+import {EventThumbnailComponent} from  './events/event-thumbnail.component'
+import {NavBarComponent} from './nav/navbar.component'
+
+
+@NgModule({
+    imports:[BrowserModule],
+    declarations: [EventsAppComponent, EventListComponent, EventThumbnailComponent, NavBarComponent],
+    bootstrap: [EventsAppComponent]
+})
+export class AppModule {}

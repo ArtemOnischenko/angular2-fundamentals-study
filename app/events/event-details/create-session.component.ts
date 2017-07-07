d8e4b94af1eefba15886c9827e72e12cb558b9f4
onischenkoa <artem.onischenko@ligazakon.ua>
2017-07-07 15:02:24 +0300

    reusable custom  Validator

--- app/events/event-details/create-session.component.ts
+++ app/events/event-details/create-session.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from "@angular/core";
 import {FormControl, Validators, FormGroup} from "@angular/forms";
+import {restrictedWords} from "../shared/restricted-words.validator";
 
 @Component({
     templateUrl: 'app/events/event-details/create-session.component.html',
@@ -21,7 +22,7 @@ export class CreateSessionComponent implements OnInit{
         this.presenter = new FormControl('', Validators.required);
         this.duration = new FormControl('', Validators.required);
         this.level = new FormControl('', Validators.required);
-        this.abstract = new FormControl('', [Validators.required, Validators.maxLength(400)]);
+        this.abstract = new FormControl('', [Validators.required, Validators.maxLength(400), restrictedWords(['foo', 'bar'])]);
 
         this.newSessionForm = new FormGroup({
             name: this.name,
